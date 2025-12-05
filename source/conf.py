bb1546a82e93c08d04988b521353d89922de0248
bakr.aassoul <sorry.no.sauce.foru@gmail.com>
2025-12-05 16:20:59 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -26,7 +26,7 @@ latex_engine = 'xelatex'
 latex_additional_files = ['fh-dortmund-logo.jpg']
 
 latex_elements = {
-    'pointsize': '12pt',
+    'pointsize': '13pt',
     'sphinxsetup': 'verbatimwithframe=true, verbatimwrapslines=true',
     'preamble': r'''
         % XeLaTeX handles UTF-8 natively, so inputenc/fontenc not needed
