09bd7a4959aca285838e7b5b793d5f1269ab37eb
bakr.aassoul <sorry.no.sauce.foru@gmail.com>
2025-12-05 13:22:45 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ html_static_path = ['_static']
 latex_engine = 'xelatex'
 
 # Tell Sphinx to copy the logo into the LaTeX build dir
-latex_additional_files = ['fh-dortmund-logo.png']
+latex_additional_files = ['fh-dortmund-logo.jpg']
 
 latex_elements = {
     'sphinxsetup': 'verbatimwithframe=true, verbatimwrapslines=true',
@@ -48,7 +48,7 @@ latex_elements = {
         \begin{flushleft}
             % Replace 'fh-dortmund-logo.png' with the actual filename of your image.
             % Adjust 'width=0.2\textwidth' to change the logo's size.
-            \includegraphics[width=0.2\textwidth]{fh-dortmund-logo.png}
+            \includegraphics[width=0.2\textwidth]{fh-dortmund-logo.jpg}
         \end{flushleft}
         \vspace*{2cm}
         \centering
